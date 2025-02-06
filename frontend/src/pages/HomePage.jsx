6c007e4959d501af7c02391de252120ece8af3cc
sungjoon92 <wsjtjdwns92@naver.com>
2025-02-06 16:23:27 +0900

    test

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -5,7 +5,6 @@ import Article from "../components/Article";
 
 export default function HomePage() {
   const [articles, setArticles] = useState([]);
-  const [isDeleted, setIsDeleted] = useState(false);
 
   async function fetchArticles() {
     const response = await articlesApi.getArticles();
@@ -17,12 +16,11 @@ export default function HomePage() {
     setArticles((prevArticles) =>
       prevArticles.filter((article) => article.id !== id)
     );
-    setIsDeleted((prev) => !prev); // 상태 변경
   };
 
   useEffect(() => {
     fetchArticles();
-  }, [isDeleted]); // isDeleted가 변경될 때마다 데이터 다시 가져오기
+  }, []); // isDeleted가 변경될 때마다 데이터 다시 가져오기
 
   return (
     <div>
